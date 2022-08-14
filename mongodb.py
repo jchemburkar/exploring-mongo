9bb49c21b938d39498bf6eabd558f33467039b2b
jchemburkar <jchemburkar@Jonathans-MacBook-Pro.local>
2022-08-14 13:59:12 -0400

    add simple writes

--- mongodb.py
+++ mongodb.py
@@ -13,11 +13,36 @@ def get_db_url() -> str:
     return url
 
 
-def connect_to_database() -> pymongo.MongoClient:
+def connect_to_database() -> pymongo.mongo_client.MongoClient:
     """ create connection using UN+PW """
     url = get_db_url()
     client = client = pymongo.MongoClient(url)
     return client
+
+
+########################################
+# database creation + insert test data #
+########################################
+
+def get_database(client: pymongo.mongo_client.MongoClient, database_name: str) -> pymongo.database.Database:
+    """ use the client to call on a database -- it will create it if it doesnt exist! """
+    database = client[database_name]
+    return database
+
+
+def get_collection(database: pymongo.database.Database, collection_name: str) -> pymongo.collection.Collection:
+    """ use the client to get a collection -- it will create it if it doesnt exist! """
+    collection = database[collection_name]
+    return collection
+
+
+def insert_test_data(collection: pymongo.collection.Collection) -> None:
+    """ """
+    sample_data = {"message": "hello world", "author": "jchem"}
+    more_sample_data = {"message": "this is a test", "author": "jonathan"}
+    collection.insert_one(sample_data)
+    collection.insert_one(more_sample_data)
+
 
 ########
 # main #
@@ -25,7 +50,10 @@ def connect_to_database() -> pymongo.MongoClient:
 
 def main():
     """ main func """
-    connect_to_database()
+    client = connect_to_database()
+    database = get_database(client, "testdb")
+    collection = get_collection(database, "testcollection")
+    insert_test_data(collection)
 
 
 if __name__ == "__main__":
